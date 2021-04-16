9aac5e18f80f41801739bf06507004ecd6d76928
anubajaj2 <anurag.bajaj02@gmail.com>
2021-04-16 11:22:38 +0530

    anubhav changes

---
+++ generators/app/index.js
@@ -0,0 +1,84 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const path = require("path");
+const glob = require("glob");
+
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: "input",
+        name: "projectName",
+        message: "What project name would you like?",
+        validate: (s) => {
+          if (/^[a-zA-Z0-9_-]*$/g.test(s)) {
+            return true;
+          }
+
+          return "Please only use alphanumeric characters for the project name.";
+        },
+        default: "anubhavsacfiori",
+      },
+      {
+        type: "confirm",
+        name: "newDir",
+        message: "Would you like to create a new directory for this project?",
+        default: true,
+      },
+      {
+        type: "input",
+        name: "projectNamespace",
+        message: "What is the namespace of your project?",
+        default: "anubhav.sac",
+      },
+      {
+        type: "input",
+        name: "projectNamespaceModule",
+        message: "What is the namespace of your project?",
+        default: "anubhav/sac",
+      },
+      {
+        type: "input",
+        name: "sac_app_url",
+        message: "URL of your SAP Analytics Cloud Story/Application",
+        default: "https://anubhavtrainings.com",
+      },
+    ]).then((answers) => {
+      if (answers.newDir) {
+        this.destinationRoot(`${answers.projectName}`);
+      }
+      this.config.set(answers);
+    });
+  }
+
+  writing() {
+    this.sourceRoot(path.join(__dirname, "templates"));
+    glob
+      .sync("**", {
+        cwd: this.sourceRoot(),
+        nodir: true,
+      })
+      .forEach((file) => {
+        const sOrigin = this.templatePath(file);
+        const sTarget = this.destinationPath(file);
+        this.fs.copyTpl(sOrigin, sTarget, this.config.getAll());
+      });
+  }
+
+  install() {
+    /*
+    This.installDependencies({
+      bower: false,
+      npm: true
+    });
+    */
+  }
+
+  end() {
+    this.log("");
+    this.log(
+      "Thanks for using, please visit us on https://www.anubhavtrainings.com!!"
+    );
+    this.log("");
+  }
+};
